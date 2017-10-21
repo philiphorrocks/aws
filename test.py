ade83a07cf6f1ea9a7c2d633d4875a2d2fbed900
Philip Horrocks <phil@philips-air.home>
2017-10-21 14:15:37 +0100

    Updated list

--- test.py
+++ test.py
@@ -5,10 +5,13 @@ import boto3
 ec2client = boto3.client('ec2')
 response = ec2client.describe_instances()
 
+# Get list of all EC2 instances
+
 for reservation in response["Reservations"]:
     for instance in reservation["Instances"]:
         # This sample print will output entire Dictionary object
         print(instance)
+
         # This will print will output the value of the Dictionary key 'InstanceId'
         print("Instance Name: " + instance["InstanceId"])
         print("Public DNS: " + instance["PublicDnsName"])
