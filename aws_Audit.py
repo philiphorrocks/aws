559e91645409415d3f3e675785dd51e6e329ce88
Philip Horrocks <phil@philips-air.home>
2017-11-06 21:11:14 +0000

    updated

--- aws_Audit.py
+++ aws_Audit.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import boto3
 
 
@@ -6,27 +7,39 @@ def awsInstanceDetails():
     # Connect to EC2 client
 
     ec2client = boto3.client('ec2')
-    response = ec2client.describe_instances()
+    response1 = ec2client.describe_instances()
 
     # Get list of all EC2 instances
     print("Getting list of EC2 instances:\n")
 
-    for reservation in response["Reservations"]:
+    for reservation in response1["Reservations"]:
         for instance in reservation["Instances"]:
 
             try:
                 # This sample print will output entire Dictionary object
-                print(instance)
-
+                #print(instance)
+                id = instance["InstanceId"]
+                print(id)
                 # This will print will output the value of the Dictionary key 'InstanceId'
                 print("Instance Name: " + instance["InstanceId"])
                 print("Public DNS: " + instance["PublicDnsName"])
                 print("Zone: " + instance["Placement"]["AvailabilityZone"])
 
+                return instance["PublicDnsName"]
 
             except:
 
                 print("Cannot get AWS instance details")
+
+
+def awsGetVolumes():
+
+    ec2 = boto3.resource('ec2')
+
+    for i in ec2.instances.all():
+
+
+        print("Id: " + i.id + "\nState: " + i.state['Name'])
 
 
 def s3StorageDetails():
@@ -53,6 +66,4 @@ def s3StorageDetails():
 
 # Main Method
 if __name__ == '__main__':
-
     awsInstanceDetails()
-    s3StorageDetails()
